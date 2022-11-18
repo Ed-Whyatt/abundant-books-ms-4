3fb17576b90baf56874642a2ebd60665e8a3e027
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-18 18:26:42 +0000

    Add function index in home view to render home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    A view to return home index page
+    """
+    return render(request, 'home/index.html')
