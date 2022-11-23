987d4b32d024d069411294dd42c5f79096bc05aa
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-23 17:20:19 +0000

    Fix base navgation and header for mobile top header

--- abundant_books/urls.py
+++ abundant_books/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-] + static(settings.MEDIA_URL, document_roosettings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
