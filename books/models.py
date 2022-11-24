dc23b821d0d6c244e2f03c374a79e54f0aa7741e
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-24 18:08:42 +0000

    Add category and book models for book app

--- books/models.py
+++ books/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def __str__(self):
+        return self.friendly_name
+
+
+class Book(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    author = models.CharField(max_length=254, null=True, blank=True)
+    format = models.CharField(max_length=254, null=True, blank=True)
+    book_depository_stars = models.DecimalField(max_digits=6, decimal_places=2,
+                                                null=True, blank=True)
+    price = models.CharField(max_length=254)
+    currency = models.TextField(null=True, blank=True)
+    old_price = models.DecimalField(max_digits=6, decimal_places=2,
+                                    null=True, blank=True)
+    isbn = models.DecimalField(max_digits=15, decimal_places=0,
+                               null=True, blank=True)
+    img_paths = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
