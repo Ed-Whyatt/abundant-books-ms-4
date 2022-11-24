cb4bbba7c506850c4822521da088a1d549375993
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-24 19:06:11 +0000

    Add books views, template, urls and django settins

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def all_books(request):
+    """
+    A view to return all book products, including sorting and search queries
+    """
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'books/books.html', context)
