7a3e6965168a9b9fa3db880c9a624ab8afe2c9b8
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-25 21:14:07 +0000

    Add serach functionality for book search

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
 
@@ -7,9 +9,22 @@ def all_books(request):
     A view to return all book products, including sorting and search queries
     """
     books = Book.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               "You didn't enter any search criteria!")
+                return redirect(reverse('books'))
+
+            queries = Q(name__icontains=query) | Q(author__icontains=query)
+            books = books.filter(queries)
 
     context = {
         'books': books,
+        'search_term': query,
     }
 
     return render(request, 'books/books.html', context)
