b7a4f775ce74ae30c58f5b82f72369da0fe48125
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-28 17:13:03 +0000

    Add sorting for book products navgation

--- books/views.py
+++ books/views.py
@@ -11,6 +11,23 @@ def all_books(request):
     books = Book.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                books = books.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            books = books.order_by(sortkey)
 
     if 'category' in request.GET:
         categories = request.GET['category'].split(',')
@@ -28,10 +45,13 @@ def all_books(request):
             queries = Q(name__icontains=query) | Q(author__icontains=query)
             books = books.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'books': books,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'books/books.html', context)
