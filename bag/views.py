354e03d4b8f891868a3d55d6ec697d799ebc2a38
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-29 18:27:09 +0000

    Add message template literals for toasts

--- bag/views.py
+++ bag/views.py
@@ -23,12 +23,12 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        # messages.success(request,
-        #                  (f'Updated {product.name} '
-        #                   f'quantity to {bag[item_id]}'))
+        messages.success(request,
+                         (f'Updated {book.name} '
+                          f'quantity to {bag[item_id]}'))
     else:
         bag[item_id] = quantity
-        # messages.success(request, f'Added {product.name} to your bag')
+        messages.success(request, f'Added {book.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -83,10 +83,22 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
-        # messages.success(request, (f'Updated {product.name} 'f'quantity to {bag[item_id]}'))
+        messages.success(request, (f'Updated {book.name} 'f'quantity to {bag[item_id]}'))
     else:
         bag.pop(item_id)
-        # messages.success(request, (f'Removed {product.name} 'f'from your bag'))
+        messages.success(request, (f'Removed {book.name} 'f'from your bag'))
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    book = get_object_or_404(Book, pk=item_id)
+    bag = request.session.get('bag', {})
+
+    bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
