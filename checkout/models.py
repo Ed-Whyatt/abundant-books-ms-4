bd85209a338affc444e6bc9fad734028bd10b723
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-30 12:19:10 +0000

    Add imports for checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,13 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from django_countries.fields import CountryField
+
+from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
