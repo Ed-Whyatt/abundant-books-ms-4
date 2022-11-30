8a1316ac00e673ac3fe13f3eecd26e3fd554bac1
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-30 12:17:24 +0000

    Add order line item models for book orders

--- checkout/models.py
+++ checkout/models.py
@@ -25,3 +25,15 @@ class Order(models.Model):
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False,
                                   blank=False, default='')
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    product = models.ForeignKey(Product, null=False,
+                                blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
