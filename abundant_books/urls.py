b0dd487b314aa90f929f17650c61b8d84aeaf8f2
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-30 16:54:37 +0000

    Add basic views for checkout

--- abundant_books/urls.py
+++ abundant_books/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('books/', include('books.urls')),
     path('bag', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
