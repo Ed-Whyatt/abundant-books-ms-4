6affd70b66a86647e451d4e533d94a8ec3c2234d
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-30 12:34:18 +0000

    Add functions fo ordeline r models

--- checkout/models.py
+++ checkout/models.py
@@ -79,3 +79,14 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
                                          null=False, blank=False,
                                          editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
