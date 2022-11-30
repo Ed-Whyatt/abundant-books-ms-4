43019b0b066ee2f7203e253f8a38ab4926646706
Ed Whyatt <lizardxyz@googlemail.com>
2022-11-30 20:59:40 +0000

    Add stripe javascript for checkout

--- abundant_books/settings.py
+++ abundant_books/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
