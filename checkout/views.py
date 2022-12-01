6b0b333352a99db2b6a392fa13cdcdee090ff8d2
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-01 11:41:57 +0000

    Add view function for successful checkout

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,7 @@ from bag.contexts import bag_contents
 import stripe
 
 
+# Checkout request for stripe
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -98,3 +99,25 @@ def checkout(request):
         }
 
         return render(request, template, context)
+
+
+# Checkout successful for stripe checkouts
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
