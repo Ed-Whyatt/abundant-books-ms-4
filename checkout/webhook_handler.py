2435c263cbbdbe298c96c7c67d097cbf59b27649
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-02 13:12:33 +0000

    Add update profile information for webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from books.models import Book
+from profiles.models import UserProfile
 
 import json
 import time
-
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -25,7 +25,6 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
-
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag
@@ -44,6 +43,21 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -77,6 +91,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -88,8 +103,8 @@ class StripeWH_Handler:
                     original_bag=bag,
                     stripe_pid=pid,
                 )
-                book = Book.objects.get(id=item_id)
-                if isinstance(item_data, int):
+                for item_id, item_data in json.loads(bag).items():
+                    book = Book.objects.get(id=item_id)
                     order_line_item = OrderLineItem(
                         order=order,
                         book=book,
