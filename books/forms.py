9e721326e5f979c4b4f7c2f5fe2e1567ca1b6c92
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-08 16:03:02 +0000

    Add crispy for html for add book product

--- books/forms.py
+++ books/forms.py
@@ -1,15 +1,12 @@
 from django import forms
-from .widgets import CustomClearableFileInput
 from .models import Book, Category
 
 
-class ProductForm(forms.ModelForm):
+class BookForm(forms.ModelForm):
 
     class Meta:
-        model = book
+        model = Book
         fields = '__all__'
-
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- books/models.py
+++ books/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def __str__(self):
+    def get_friendly_name(self):
         return self.friendly_name
 
 
