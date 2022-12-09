b857f2d5f9d12caabc28b09be022cbd799be00a2
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-09 16:20:40 +0000

    Add submit post method view for newsletter form

--- news_letter/views.py
+++ news_letter/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from .forms import NewsLetterForm
 
 
 def news_letter(request):
-    """ A view to return news letter page """
-    form = NewsLetterForm()
+    """ A view to render news letter page and submit the form """
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Succesfully added to the News Letter!')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Failed to submit the Nes Letter form please ensure the formm is valid.')
+    else:
+        form = NewsLetterForm()
+
     template = 'news_letter.html'
     context = {
         'form': form,
