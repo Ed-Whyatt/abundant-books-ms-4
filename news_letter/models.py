ea0ef5de8d50e11a5f061dc47023fc5846014b52
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-09 14:22:58 +0000

    Add and migrate model for news letter

--- news_letter/models.py
+++ news_letter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class News_Letter(models.Model):
+    """
+    A user news mailing list for the new letter
+    """
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
