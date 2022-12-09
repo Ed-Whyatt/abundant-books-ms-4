0c01138c5e7f4b0d6f10eedfebb0f0ed9a75f616
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-09 13:40:32 +0000

    Move  htmljavascipt  to js files and add links to files

--- static/js/postload_base.js
+++ static/js/postload_base.js
@@ -19,3 +19,59 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// <!-- Toasts JS -->
+let toastElList = [].slice.call(document.querySelectorAll('.toast'))
+let toastList = toastElList.map(function (toastEl) {
+    let option = {
+         animation: true,
+         autohide: true,
+         delay: 10000,
+    }
+    let bsToast = new bootstrap.Toast(toastEl, option)
+    bsToast.show();
+})
+
+// <!-- Script for back to top button -->
+$('.btt-link').click(function(e) {
+    window.scrollTo(0, 0)
+})
+
+// <!-- Script for book category, price, rating, and name order select -->
+$('#sort-selector').change(function () {
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.val();
+    console.log(selectedVal)
+    if (selectedVal == "book_depository_stars_asc") {
+        var sort = "book_depository_stars";
+        var direction = "desc";
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else if (selectedVal == "book_depository_stars_desc") {
+        var sort = "book_depository_stars";
+        var direction = "asc";
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else if (selectedVal != "reset") {
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl);
+    }
+})
