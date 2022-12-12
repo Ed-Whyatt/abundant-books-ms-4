d89fbe3b2a8d41c37c2615bcd17ccfbbc1256793
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-12 12:04:55 +0000

    Add settings and procfile for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn abundant_books.wsgi:application
--- abundant_books/settings.py
+++ abundant_books/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 if os.path.exists("env.py"):
     import env
@@ -31,7 +32,7 @@ development = os.environ.get('DEVELOPMENT')
 
 DEBUG = development
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME"), 'localhost']
 
 
 # Application definition
@@ -128,12 +129,17 @@ WSGI_APPLICATION = 'abundant_books.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASE = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
