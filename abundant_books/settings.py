21a17ecc47e543192ca91545753732c3f0099587
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-12 14:23:10 +0000

    Add aws cache controls

--- abundant_books/settings.py
+++ abundant_books/settings.py
@@ -186,6 +186,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'abundant-books-ms4'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
