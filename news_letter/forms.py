0609f96ae32c73ba445e8608007c017d1e74c766
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-22 23:09:39 +0000

    Add style and form placeholders for news letter page

--- news_letter/forms.py
+++ news_letter/forms.py
@@ -6,3 +6,20 @@ class NewsLetterForm(forms.ModelForm):
     class Meta:
         model = News_Letter
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address'
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
