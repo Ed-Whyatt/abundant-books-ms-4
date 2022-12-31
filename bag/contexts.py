727e1c0ac87b95104fd822ee1e8c07a60895f15d
Ed Whyatt <lizardxyz@googlemail.com>
2022-12-31 12:34:37 +0000

    Fix bag and checkout total

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,7 +13,8 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         book = get_object_or_404(Book, pk=item_id)
-        total += quantity * float(book.price)
+        total += quantity * Decimal(book.price)
+
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
@@ -22,13 +23,13 @@ def bag_contents(request):
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = int(total) * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + int(total)
+    grand_total = delivery + total
 
     context = {
         'bag_items': bag_items,
--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,9 +1,9 @@
 from django import template
-
+from decimal import Decimal
 
 register = template.Library()
 
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
-    return float(price) * quantity
+    return Decimal(price) * quantity
