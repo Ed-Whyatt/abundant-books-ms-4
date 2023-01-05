39bdaa05baa3e44a2b984ee254bd2718aaca9a27
Ed Whyatt <lizardxyz@googlemail.com>
2023-01-05 23:00:42 +0000

    Add python tests for home app

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHomeView(TestCase):
+
+    # Test view returns 200 response
+    def test_view_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
