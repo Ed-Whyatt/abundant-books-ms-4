aa8bf84fdc788b47c468115b34ea8f50cd55c66e
Ed Whyatt <lizardxyz@googlemail.com>
2023-01-05 22:59:00 +0000

    Add python tests for bag app

--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestBagView(TestCase):
+
+    # Test view returns 200 response
+    def test_view_bag(self):
+        response = self.client.get('/bag/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bag/bag.html')
